400e618b9e9f98a4eb70e4044f4803d092aacf41
Amaurí Geraldes <amaurimgeraldes@gmail.com>
2024-01-28 18:41:10 -0300

    Update __init__.py
    Fazendo os ajustes para a Criação das Tabelas no Banco de Dados do railway

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -23,6 +23,9 @@ from flask_login import LoginManager
 # Importando a biblioteca os
 import os
 
+# # Importando a biblioteca sqlalchemy
+import sqlalchemy
+
 # Criando uma instância da Flask classe
 app = Flask(__name__)
 
@@ -60,6 +63,29 @@ login_manager.login_view = "login"
 login_manager.login_message = "Favor efetuar Login ou Criar Conta para ter acesso"
 # Ver as Categorias de Alertas em https://getbootstrap.com/docs/5.0/components/alerts/
 login_manager.login_message_category = "alert-info"
+
+# Importando "models.py"
+# Obs.: Para carregar as Tabelas que precisarão estar contidas no Banco de Dados
+from comunidadeimpressionadora import models
+# Criando uma engine para avaliar o Banco de Dados
+# Obs.: A variável "engine" recebe o Link do Banco de Dados
+engine = sqlalchemy.create_engine(os.getenv("DATABASE_URL"))
+# Verificando dentro desta engine (Banco de Dados) se ela tem a Tabela de Usuários
+inspector = sqlalchemy.inspect(engine)
+# Se dentro do inspector, ou seja, da engine não tem a Tabela usuario
+# Obs.: Mesmo que o nome da Classe seja Usuario, utilizar em letras minúsculas
+if not inspector.has_table("usuario"):
+    # Criando o contexto para não gerar a mensagem de erro
+    with app.app_context():
+        # Caso tenha criado um Banco de Dados que não tenha a tabela usuario
+        # Obs.: Se for criar e já existir gerará um Erro
+        database.drop_all()
+        # Criando o Banco de Dados
+        database.create_all()
+        print("Base de Dados criada")
+# Caso contrário
+else:
+    print("Base de Dados já existente")
 
 
 # Importando "routes.py" para colocar os Links no ar
